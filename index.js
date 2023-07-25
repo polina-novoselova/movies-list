b1dbf15539b49e6223f42bb0496afffd2bab54e3
Polina Novoselova <polka.nov@mail.ru>
2023-07-25 20:32:34 +0200

    add checkbox and heis styles

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const moviesListNode = document.getElementById("movie-items");
 
 let moviesList = [];
 
-init();
+// init();
 
 function addMovie() {
   const filmName = getMovieNameFromUser();
@@ -15,55 +15,51 @@ function addMovie() {
   saveMoviesToStorage();
   clearInput();
   renderMovies();
-}
+};
 
 function getMovieNameFromUser() {
   const filmName = inputMovieAdderNode.value;
   return filmName;
-}
+};
 
 function setMovie(filmName, movieId) {
   moviesList.push({
     name: filmName,
     id: movieId,
-    checked: false,
+    // checked: false,
   });
-}
+};
 
 function saveMoviesToStorage() {
   localStorage.setItem("movies", JSON.stringify(moviesList));
-}
+};
 
 function getMoviesFromStorage() {
   const savedMovies = localStorage.getItem("movies");
   if (savedMovies) {
     moviesList = JSON.parse(savedMovies);
-    moviesList.forEach((movie) => {
-      movie.checked = !!movie.checked;
-    });
   }
-}
+};
 
 function clearInput() {
   inputMovieAdderNode.value = "";
-}
+};
 
 function renderMovies() {
   moviesListNode.innerHTML = "";
   moviesList.forEach((movie) => {
     const movieItem = document.createElement("li");
-    movieItem.className = `movie-item ${movie.checked ? "checked" : ""}`;
+    movieItem.className = "movie-item";
     movieItem.setAttribute("id", movie.id);
 
     const movieCheckbox = document.createElement("input");
     movieCheckbox.className = "checkbox";
-    movieCheckbox.setAttribute("id", movie.id);
     movieCheckbox.setAttribute("type", "checkbox");
+    movieCheckbox.setAttribute("checked", "false");
     movieCheckbox.checked = movie.checked;
 
     const movieLabel = document.createElement("label");
     movieLabel.className = "label";
-    movieLabel.setAttribute("for", movie.id);
 
     const movieName = document.createElement("span");
     movieName.className = "movie-name";
@@ -74,98 +70,85 @@ function renderMovies() {
     deleteMovieBtn.setAttribute("id", movie.id);
     deleteMovieBtn.setAttribute("title", "Удалить");
 
-    movieItem.appendChild(movieCheckbox);
+    movieLabel.appendChild(movieCheckbox);
     movieItem.appendChild(movieLabel);
     movieLabel.appendChild(movieName);
     movieItem.appendChild(deleteMovieBtn);
 
     moviesListNode.appendChild(movieItem);
   });
-}
+};
 
 function init() {
   getMoviesFromStorage();
   renderMovies();
-}
-
-function hoverOnCheckbox(event) {
-  const target = event.target;
-
-  if (target.matches("li") || target.closest("li")) {
-    const checkbox = target.querySelector(".checkbox");
-
-    if (checkbox) {
-      checkbox.classList.add("hovered");
-    }
-  }
-}
-
-function hoverOffCheckbox(event) {
-  const target = event.target;
-
-  if (target.matches("li") || target.closest("li")) {
-    const checkbox = target.querySelector(".checkbox");
-
-    if (checkbox) {
-      checkbox.classList.remove("hovered");
-    }
-  }
-}
-
-function checkboxChecked(event) {
-  const target = event.target;
-
-  const movieItem = target.closest(".movie-item");
-
-  if (movieItem) {
-    const deleteBtn = target.closest(".btn-delete-item");
-
-    if (deleteBtn) {
-      return;
-    }
-
-    const checkbox = movieItem.querySelector(".checkbox");
-
-    if (checkbox) {
-      checkbox.classList.toggle("checked");
-      movieItem.classList.toggle("checked");
-      updateMovieCheckedState(movieItem.id, checkbox.checked);
-    }
-  }
-}
-
-function updateMovieCheckedState(movieId, checked) {
-  const movie = moviesList.find((movie) => movie.id === movieId);
-  if (movie) {
-    movie.checked = checked;
-    saveMoviesToStorage();
-    renderMovies();
-  }
 };
 
-function deleteMovie(event) {
-    const target = event.target;
-  
-    const deleteBtn = target.closest(".btn-delete-item");
-  
-    if (deleteBtn) {
-      const movieItem = deleteBtn.closest(".movie-item");
-      const movieId = movieItem.id;
-  
-      // Удалить фильм из списка
-      removeMovie(movieId);
-      saveMoviesToStorage();
-      renderMovies();
+document.addEventListener("DOMContentLoaded", function () {
+  const checkboxes = document.querySelectorAll(".checkbox input");
+
+  checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener("click", function () {
+      this.setAttribute("checked", this.checked);
+    });
+  });
+});
+
+document.addEventListener("click", function (event) {
+  const target = event.target;
+
+  if (target.classList.contains("checkbox")) {
+    const checkbox = target.querySelector("input");
+
+    if (target.classList.contains("active")) {
+      checkbox.checked = false;
+    } else {
+      checkbox.checked = true;
     }
-};
 
-function removeMovie(movieId) {
-    moviesList = moviesList.filter((movie) => movie.id !== movieId);
-};
+    target.classList.toggle("active");
+    event.preventDefault();
+  }
+});
 
+// // Получаем все элементы с классом "movies-item"
+// const movieItems = document.querySelectorAll('.checkbox');
+
+// // Функция для обработки клика 
+// function handleMovieItemClick(event) {
+//   // Проверяем был ли клип что бы не менять чекед checked
+//   if (!event.target.matches('input[type="checkbox"]')) {
+//     // Добавляем класс актив к карточке и дочкам
+//     this.classList.toggle('active');
+//     // Ищем чекбокс внутри карточки и меняем его атрибут checked с false на true
+//     const checkbox = this.querySelector('input[type="checkbox"]');
+//     checkbox.checked = !checkbox.checked;
+//   }
+// }
+// movieItems.forEach(item => {
+//   item.addEventListener('click', handleMovieItemClick);
+// });
+
+
+// function deleteMovie(event) {
+//     const target = event.target;
+
+//     const deleteBtn = target.closest(".btn-delete-item");
+
+//     if (deleteBtn) {
+//       const movieItem = deleteBtn.closest(".movie-item");
+//       const movieId = movieItem.id;
+
+//       // Удалить фильм из списка
+//       removeMovie(movieId);
+//       saveMoviesToStorage();
+//       renderMovies();
+//     }
+// };
+
+// function removeMovie(movieId) {
+//     moviesList = moviesList.filter((movie) => movie.id !== movieId);
+// };
 
 btnAddMovieNode.addEventListener("click", addMovie);
-moviesListNode.addEventListener("click", checkboxChecked);
-moviesListNode.addEventListener("mouseover", hoverOnCheckbox);
-moviesListNode.addEventListener("mouseout", hoverOffCheckbox);
-moviesListNode.addEventListener("click", deleteMovie, true);
+// moviesListNode.addEventListener("click", deleteMovie, true);
